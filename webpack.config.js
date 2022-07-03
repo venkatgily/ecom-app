b623d0907d76193c1d3f8b41fd9413fa784deb35
Venkateshwaran Shankaranarayanan <venkateshwaran.s@VSCONNECT.NET>
2022-07-03 23:54:57 +0530

    correction

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,26 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: path.join(__dirname, "src", "index.js"),
+    entry: path.join(__dirname, "src", "index.jsx"),
     output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
     mode: process.env.NODE_ENV || "development",
-    resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.css', '.scss'],
+        modules: [path.resolve(__dirname, "src"), "node_modules"]
+    },
     devServer: { static: path.join(__dirname, "src") },
-    module: {
+    module: { 
         rules: [
-            { 
-                test: /\.(js|jsx)$/, 
-                exclude: /node_modules/, 
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
                 use: ["babel-loader"]
             },
             {
                 test: /\.(css|scss)$/,
                 use: ["style-loader", "css-loader"],
             },
-            { 
+            {
                 test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
                 use: ["file-loader"]
             },
